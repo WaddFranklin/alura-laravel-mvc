46ac8a5bbb0793895b09d65c0a9faa6d2d9dcabf
WaddFranklin <waddinsohn@gmail.com>
2022-12-03 10:15:08 -0300

    first commit

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,9 @@
+<x-layout title="Nova Série">
+    <form action="" method="post">
+        <div class="mb-3">
+            <label class="form-label" for="nome">Nome:</label>
+            <input class="form-control" type="text" name="nome" id="nome">
+        </div>
+        <button class="btn btn-primary" type="submit">Adicionar</button>
+    </form>
+</x-layout>
